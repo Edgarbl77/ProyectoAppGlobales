74dc539186e8fbdc0832cb9c0d28ae8a70ffac3d
RobertoMJ <robertomj92@hotmail.com>
2015-10-24 19:27:53 -0600

    Plantilla incial para los reportes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,20 @@
  */
 angular.module('proyectoAppGlobalesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.reportes = [
+      {
+        img: 'images/poste.jpg',
+        titulo: 'Poste de luz dañado',
+        descripcion: 'Un poste de luz se ha caído y no hay iluminación',
+        dirección: 'Heredia centro',
+        fecha: new Date('2015', '23', '10')
+      },
+      {
+        img: 'images/tuberia.jpg',
+        titulo: 'Fuga de agua',
+        descripcion: 'Una tubería esta rota y se esta desperdiciando mucha agua',
+        dirección: 'Barva, Heredia',
+        fecha: new Date('2015', '23', '10')
+      }
     ];
   });
